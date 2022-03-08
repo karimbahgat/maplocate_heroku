c38b1af8b4997daae18bc4386ce6b62820d2fdaa
Karim Bahgat <karim.bahgat.norway@gmail.com>
2022-03-08 20:54:10 +0100

    Fix post data receiving

--- maplocate_heroku/views.py
+++ maplocate_heroku/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 import json
 
+@csrf_exempt
 def run_action(request):
     print('job request received')
 
-    # get input data as json dict passed through environment variable
-    action = request.POST['MAPLOCATE_ACTION']
-    mapid = request.POST['MAPLOCATE_MAPID']
-    respond_to = request.POST['MAPLOCATE_HOST']
-    data = json.loads(request.POST['MAPLOCATE_KWARGS'])
+    # get input data as json dict passed through body (POST)
+    POST = json.loads(request.body)
+    action = POST['MAPLOCATE_ACTION']
+    mapid = POST['MAPLOCATE_MAPID']
+    respond_to = POST['MAPLOCATE_HOST']
+    data = json.loads(POST['MAPLOCATE_KWARGS'])
     print(action,mapid,respond_to,data)
 
     import run_job
